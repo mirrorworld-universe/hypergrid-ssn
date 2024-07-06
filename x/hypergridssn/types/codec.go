98900bc87b7e4ec0999fe88438ca5234309c0356
ZeneDeLuca <zenedeluca@gmail.com>
2024-07-06 13:06:47 +0000

    chore: Add GridInbox keys and messages

--- x/hypergridssn/types/codec.go
+++ x/hypergridssn/types/codec.go
@@ -24,6 +24,9 @@ func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgCreateFeeSettlementBill{},
 	)
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgCreateGridInbox{},
+	)
 	// this line is used by starport scaffolding # 3
 
 	registry.RegisterImplementations((*sdk.Msg)(nil),
